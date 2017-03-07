0e60068b243c0575ec8b78a2334bbbd46ef07e2f
andrewsosa001 <andrewsosa001@gmail.com>
2017-03-07 17:39:51 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+allstudents-tech
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var build = 'dist';
+
+// Compiles .pug files into .html
+gulp.task('pug', () => {
+    var pug = require('gulp-pug');
+    gulp.src('src/views/*.pug')
+		.pipe(pug().on('error', console.log))
+        .pipe(gulp.dest(build));
+});
+
+// Compiles .sass into .css
+gulp.task('sass', function() {
+    var sass = require('gulp-sass');
+    gulp.src('src/assets/sass/*.sass')
+        .pipe(sass().on('error', sass.logError))
+    	.pipe(gulp.dest(build + '/css/'));
+});
+
+// Copies javascript into build
+gulp.task('js', function() {
+    gulp.src('src/assets/js/*')
+        .pipe(gulp.dest(build + '/js/'))
+    gulp.src('src/*.js')
+        .pipe(gulp.dest(build + '/js/'))
+});
+
+// Copies other assets
+gulp.task('assets', function() {
+    gulp.src('src/assets/*.css')
+        .pipe(gulp.dest(build + '/css/'))
+});
+
+
+// Removes built files
+gulp.task('clean', function() {
+    var clean = require('gulp-clean');
+    gulp.src(build + '/**', {read:false})
+        .pipe(clean());
+});
+
+// Monitors .pug and .sass
+gulp.task('watch', ['build'], function() {
+    gulp.watch('src/**', ['build']);
+});
+
+// Single time build of files
+gulp.task('build', ['pug', 'sass', 'js', 'assets']);
+gulp.task('default', ['build']);
